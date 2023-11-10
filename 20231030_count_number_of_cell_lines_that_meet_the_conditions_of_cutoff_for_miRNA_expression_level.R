1d430513534f8b04caede258993219596aa81e12
Ryosuke-Hirota <116142514+Ryosuke-Hirota@users.noreply.github.com>
2023-11-10 12:31:06 +0900

    Add files via upload

--- 20231030_count_number_of_cell_lines_that_meet_the_conditions_of_cutoff_for_miRNA_expression_level.R
+++ 20231030_count_number_of_cell_lines_that_meet_the_conditions_of_cutoff_for_miRNA_expression_level.R
@@ -18,7 +18,6 @@ miRNA.exp <-miRNA.exp[m,]
 
 miRNA.exp <-miRNA.exp[,-1]
 
-
 # log2 miRNA expression
 miRNA.exp <-as.data.frame(apply(miRNA.exp, 2, log2))
 
@@ -27,7 +26,7 @@ all.miRNA.exp.list <-as.data.frame(matrix(nrow=637,ncol=11))
 colnames(all.miRNA.exp.list) <-c("miRNA", "log2(exp)>=0", "log2(exp)>=1", "log2(exp)>=2", "log2(exp)>=3", "log2(exp)>=4",
                               "log2(exp)>=5","log2(exp)>=6","log2(exp)>=7","log2(exp)>=8","log2(exp)>=9")
 
-# count number of cell lines that meet the condtions
+# count number of cell lines that meet the conditions
 for (i in 1:nrow(miRNA.exp)) {
   all.miRNA.exp.list[i,1] <-miRNA[i]
   all.miRNA.exp.list[i,2] <-length(which(miRNA.exp[i,]>0))
@@ -52,6 +51,7 @@ colnames(all.miRNA.exp.list)[12] <-"mean expression level"
 # output
 setwd("C:/Rdata/20231019_CCLE_miRNA_cutoff")
 write.table(all.miRNA.exp.list,"CCLE_number_of_cell_lines_meet_the_conditions.txt",sep="\t",row.names = F,quote = F)
+write.table(all.miRNA.exp.list[all.miRNA.exp.list[,9]>=3,1],"list_of_CCLE_miRNA_meet_the_condition_log2_7_ÅÜ3.txt",sep="\t",row.names = F,col.names = F,quote = F)
 
 # remain only hemato cell lines
 h <-grep("HAEMATO",colnames(miRNA.exp))
@@ -62,7 +62,7 @@ hemato.miRNA.exp.list <-as.data.frame(matrix(nrow=637,ncol=11))
 colnames(hemato.miRNA.exp.list) <-c("miRNA", "log2(exp)>=0", "log2(exp)>=1", "log2(exp)>=2", "log2(exp)>=3", "log2(exp)>=4",
                               "log2(exp)>=5","log2(exp)>=6","log2(exp)>=7","log2(exp)>=8","log2(exp)>=9")
 
-# count number of cell lines that meet the condtions
+# count number of cell lines that meet the conditions
 for (i in 1:nrow(hemato.miRNA.exp)) {
   hemato.miRNA.exp.list[i,1] <-miRNA[i]
   hemato.miRNA.exp.list[i,2] <-length(which(hemato.miRNA.exp[i,]>0))
@@ -86,6 +86,7 @@ colnames(hemato.miRNA.exp.list)[12] <-"mean expression level"
 
 # output
 write.table(hemato.miRNA.exp.list,"CCLE_numer_of_hemato_cell_lines_meet_the_conditions.txt",sep="\t",row.names = F,quote = F)
+write.table(hemato.miRNA.exp.list[hemato.miRNA.exp.list[,8]>=3,1],"list_of_CCLE_hemato_miRNA_meet_the_condition_log2_6_ÅÜ3.txt",sep="\t",row.names = F,col.names = F,quote = F)
 
 # remain only liver cell lines
 l <-grep("LIVER",colnames(miRNA.exp))
@@ -96,7 +97,7 @@ liver.miRNA.exp.list <-as.data.frame(matrix(nrow=637,ncol=11))
 colnames(liver.miRNA.exp.list) <-c("miRNA", "log2(exp)>=0", "log2(exp)>=1", "log2(exp)>=2", "log2(exp)>=3", "log2(exp)>=4",
                                     "log2(exp)>=5","log2(exp)>=6","log2(exp)>=7","log2(exp)>=8","log2(exp)>=9")
 
-# count number of cell lines that meet the condtions
+# count number of cell lines that meet the conditions
 for (i in 1:nrow(liver.miRNA.exp)) {
   liver.miRNA.exp.list[i,1] <-miRNA[i]
   liver.miRNA.exp.list[i,2] <-length(which(liver.miRNA.exp[i,]>0))
@@ -120,4 +121,4 @@ colnames(liver.miRNA.exp.list)[12] <-"mean expression level"
 
 # output
 write.table(liver.miRNA.exp.list,"CCLE_numer_of_liver_cell_lines_meet_the_conditions.txt",sep="\t",row.names = F,quote = F)
-
+write.table(liver.miRNA.exp.list[liver.miRNA.exp.list[,7]>=3,1],"list_of_CCLE_liver_miRNA_meet_the_condition_log2_5_ÅÜ3.txt",sep="\t",row.names = F,col.names = F,quote = F)
